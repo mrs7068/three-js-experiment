f6d3e2ca164ba8843d80ba0e26eee1cbaac487ad
Michael <mrs7068@psu.edu>
2025-10-16 14:53:10 -0400

    Updates

---
+++ main.js
@@ -0,0 +1,128 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
+import 'style.css'
+
+
+/* import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))    */
+
+
+// Scene setup
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(50);
+camera.position.setX(-3);
+
+// Basic cube
+const geometry = new THREE.BoxGeometry(10, 10, 10);
+const material = new THREE.MeshBasicMaterial({ color: 0xFF6347 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+cube.position.z = -15;
+cube.position.x = -15;
+cube.rotation.x = 2;
+cube.rotation.y = 0.5;
+
+// Add a second object with a material that needs lighting
+const ico = new THREE.IcosahedronGeometry(10);
+const icoMaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+const icoMesh = new THREE.Mesh(ico, icoMaterial);
+scene.add(icoMesh);
+icoMesh.position.z = -15;
+icoMesh.position.x = 15;
+
+// Lights
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0, -10, 10);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+ambientLight.position.set(25, -15, -400);
+
+scene.add(pointLight);
+scene.add(ambientLight);
+
+// Helpers
+const lightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(lightHelper);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
+
+// Orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Background texture
+const spaceTexture = new THREE.TextureLoader().load('images/night_sky.jpg');
+scene.background = spaceTexture;
+
+// Texture mapping on a sphere
+const smileTexture = new THREE.TextureLoader().load('images/smile.jpg');
+const sphereGeometry = new THREE.SphereGeometry(10, 22, 10);
+const smileMaterial = new THREE.MeshBasicMaterial({ map: smileTexture });
+const smileMesh = new THREE.Mesh(sphereGeometry, smileMaterial);
+scene.add(smileMesh);
+
+// Normal mapping on a torus knot
+const normalTexture = new THREE.TextureLoader().load('images/normals/textureNormal.png');
+const torusGeo = new THREE.TorusKnotGeometry(5, 1, 250, 5, 9, 15);
+const torusMaterial = new THREE.MeshStandardMaterial({
+    normalMap: normalTexture,
+    roughness: 0,
+    metalness: 1.8,
+});
+const torusKnot = new THREE.Mesh(torusGeo, torusMaterial);
+scene.add(torusKnot);
+torusKnot.position.y = 20;
+
+// Animation loop
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    icoMesh.rotation.z += -0.03;
+    icoMesh.rotation.y += -0.03;
+
+    // rotate the smiley sphere
+    smileMesh.rotation.y += 0.05;
+
+    controls.update();
+
+    renderer.render(scene, camera);
+}
+
+// Start animation
+animate();
